e3decb25b4baa329ef3e8256828ee8a9855b51e2
ext2135 <jjimerod@ALC-C617Y52.usersad.everis.int>
2017-07-14 08:50:42 +0200

    example commit

---
+++ BECA_EXAMPLES/src/main/java/com/everis/alicante/courses/becajava/examples/CollectionExamples.java
@@ -0,0 +1,41 @@
+package com.everis.alicante.courses.becajava.examples;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class CollectionExamples {
+
+	public static void main(String[] args) {
+	
+		
+		//LIST
+		
+		List arraylist= new ArrayList<>();
+		List linkedlist= new LinkedList<>();
+		List vector= new Vector<>();
+		
+		for (int i = 0; i < 10; i++) {
+			
+			Example ex= new Example();
+			ex.setNumero(i);
+			
+			arraylist.add(ex);			
+			linkedlist.add(ex);			
+			vector.add(ex);
+		}
+		
+		
+		System.out.println(arraylist);
+		System.out.println(linkedlist);
+		System.out.println(vector);
+		
+		//metodos de linked list
+		((LinkedList<Object>)(linkedlist)).addFirst(new Example());
+		((LinkedList<Object>)(linkedlist)).getLast();
+		
+		
+	}
+
+}
