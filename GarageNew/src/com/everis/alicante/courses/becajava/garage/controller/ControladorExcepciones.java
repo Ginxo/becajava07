ca108162c64cd18b10c3857cd1b956faedaf4be0
ext2135 <jjimerod@ALC-C617Y52.usersad.everis.int>
2017-07-25 17:49:13 +0200

    default

--- GarageNew/src/com/everis/alicante/courses/becajava/garage/controller/ControladorExcepciones.java
+++ GarageNew/src/com/everis/alicante/courses/becajava/garage/controller/ControladorExcepciones.java
@@ -1,9 +1,15 @@
 package com.everis.alicante.courses.becajava.garage.controller;
 
+import com.everis.alicante.courses.becajava.garage.GarageMain;
 import com.everis.alicante.courses.becajava.garage.domain.GarajeException;
 
 public interface ControladorExcepciones {	
 	
-	void gestionaExcepcion(GarajeException exception);
+	default void gestionaExcepcion(GarajeException e) {
+		
+		e.gestionaExcepcion();
+	
+		GarageMain.iniciarAplicacion();
+	}
 	
 }
--- GarageNew/src/com/everis/alicante/courses/becajava/garage/controller/ControladorExcepcionesImpl.java
+++ GarageNew/src/com/everis/alicante/courses/becajava/garage/controller/ControladorExcepcionesImpl.java
@@ -1,22 +1,6 @@
 package com.everis.alicante.courses.becajava.garage.controller;
 
-import com.everis.alicante.courses.becajava.garage.GarageMain;
-import com.everis.alicante.courses.becajava.garage.domain.GarajeException;
-
 public class ControladorExcepcionesImpl implements ControladorExcepciones{
-
-	@Override
-	public void gestionaExcepcion(GarajeException e) {		
-		
-		e.gestionaExcepcion();
-		
-		//guarda la excepcion entera en un fichero de excepciones
-		
-		
-			GarageMain.iniciarAplicacion();
-		
-		
-	}
 
 }
 	
